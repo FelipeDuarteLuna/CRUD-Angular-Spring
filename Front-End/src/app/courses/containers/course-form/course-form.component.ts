d0bd9e4dbd453c471ce8bdf20ddda48dd07f7704
FelipeDuarteLuna <felipinho.luna@gmail.com>
2023-02-28 19:40:03 -0300

    Upgrade Angular v15 com Angular

--- Front-End/src/app/courses/containers/course-form/course-form.component.ts
+++ Front-End/src/app/courses/containers/course-form/course-form.component.ts
@@ -2,7 +2,7 @@ import { Course } from './../../model/course';
 import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { NonNullableFormBuilder, Validators } from '@angular/forms';
-import { MatLegacySnackBar as MatSnackBar } from '@angular/material/legacy-snack-bar';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { ActivatedRoute } from '@angular/router';
 
 import { CoursesService } from '../../services/courses.service';
