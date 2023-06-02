6c5881c5fdaef149ab5c7bbec7afb490c6926109
Felipe Duarte Luna <felipe.luna@totvs.com.br>
2023-06-02 08:30:49 -0300

    Correção da Atualização vAngular do MatDialogRef

--- Front-End/src/app/shared/components/error-dialog/confirmation-dialog/confirmation-dialog.component.ts
+++ Front-End/src/app/shared/components/error-dialog/confirmation-dialog/confirmation-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject, OnInit } from '@angular/core';
-import { MatLegacyDialogRef as MatDialogRef, MAT_LEGACY_DIALOG_DATA as MAT_DIALOG_DATA } from '@angular/material/legacy-dialog';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-confirmation-dialog',
