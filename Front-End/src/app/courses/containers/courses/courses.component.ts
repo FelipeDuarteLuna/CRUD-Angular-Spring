163723fa9467e6ce44b0c18f4098aafafd2895c6
Felipe Luna <felipe.luna@totvs.com.br>
2024-03-25 07:15:05 -0300

    fix(API): Front-End API Correction with Pagination

--- Front-End/src/app/courses/containers/courses/courses.component.ts
+++ Front-End/src/app/courses/containers/courses/courses.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
 import { ActivatedRoute, Router } from '@angular/router';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, Observable, of, tap } from 'rxjs';
 import { ConfirmationDialogComponent } from 'src/app/shared/components/error-dialog/confirmation-dialog/confirmation-dialog.component';
 
 import { ErrorDialogComponent } from '../../../shared/components/error-dialog/error-dialog.component';
 import { Course } from '../../model/course';
 import { CoursesService } from '../../services/courses.service';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { CoursePage } from '../../model/course-page';
 
 @Component({
   selector: 'app-courses',
@@ -17,72 +19,74 @@ import { CoursesService } from '../../services/courses.service';
 })
 export class CoursesComponent implements OnInit {
 
-  courses$: Observable<Course[]> | null = null;
+  courses$: Observable<CoursePage> | null = null;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  pageIndex = 0;
+  pageSize = 10;
 
   constructor(
     private coursesService: CoursesService,
     public dialog: MatDialog,
     private router: Router,
     private route: ActivatedRoute,
-    private _snackBar: MatSnackBar,) {
-
+    private snackBar: MatSnackBar
+  ) {
     this.refresh();
-
   }
 
-  refresh(){
-    this.courses$ = this.coursesService.listCoursesAll()
-    .pipe(
-      catchError(error =>{
-        this.onError('Erro ao carregar cursos');
-        return of([])
-      })
+  refresh(pageEvent: PageEvent = { length: 0, pageIndex: 0, pageSize: 10 }) {
+    this.courses$ = this.coursesService.list(pageEvent.pageIndex, pageEvent.pageSize)
+      .pipe(
+        tap(() => {
+          this.pageIndex = pageEvent.pageIndex;
+          this.pageSize = pageEvent.pageSize;
+        }),
+        catchError(error => {
+          this.onError('Erro ao carregar cursos.');
+          return of({ courses: [], totalElements: 0, totalPages: 0 })
+        })
       );
   }
-
 
   onError(errorMsg: string) {
     this.dialog.open(ErrorDialogComponent, {
       data: errorMsg
-    })};
-
-
-  ngOnInit(): void {
+    });
   }
+
+  ngOnInit(): void { }
 
   onAdd(){
     console.log('onAdd');
     this.router.navigate( ['new'], {relativeTo: this.route});
   }
 
-  OnEdit( course: Course){
-    this.router.navigate( ['edit', course._id], {relativeTo: this.route});
+  onEdit(course: Course) {
+    this.router.navigate(['edit', course._id], { relativeTo: this.route });
   }
 
-  onRemove( course: Course){
-
+  onRemove(course: Course) {
     const dialogRef = this.dialog.open(ConfirmationDialogComponent, {
-      data: "Tem certeza, que deseja remover esse curso ?",
+      data: 'Tem certeza que deseja remover esse curso?',
     });
 
-    dialogRef.afterClosed().subscribe( (result: boolean) => {
-      if( result){
-
-        this.coursesService.remove( course._id ).subscribe(
-          () =>{
+    dialogRef.afterClosed().subscribe((result: boolean) => {
+      if (result) {
+        this.coursesService.remove(course._id).subscribe(
+          () => {
             this.refresh();
-            this._snackBar.open("Curso removido com sucesso!", "X", {
+            this.snackBar.open('Curso removido com sucesso!', 'X', {
               duration: 5000,
               verticalPosition: 'top',
-              horizontalPosition: 'center'});
+              horizontalPosition: 'center'
+            });
           },
-          error => this.onError("Erro ao tentar remover curso !"));
-
+          () => this.onError('Erro ao tentar remover curso.')
+        );
       }
-
     });
-
   }
 
 }
-
